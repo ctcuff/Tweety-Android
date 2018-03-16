cc26d8104e2ce567f9a79e87c6a2266987505d9f
Cameron Cuff <dev.ctcuff@gmail.com>
2018-03-15 23:04:23 -0400

    Added a way to see incoming statuses

---
+++ app/src/main/java/com/camtech/android/tweetbot/data/Keys.java
@@ -0,0 +1,12 @@
+package com.camtech.android.tweetbot.data;
+
+public final class Keys {
+
+    private Keys() {}
+
+    // These keys should be private and will be different for everyone
+    public static final String CONSUMER_KEY = "";
+    public static final String CONSUMER_KEY_SECRET = "";
+    public static final String ACCESS_TOKEN = "";
+    public static final String ACCESS_TOKEN_SECRET = "";
+}
