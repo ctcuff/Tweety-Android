b147205e5b8f2483f96077c5aee99b27817bf629
Cameron Cuff <dev.ctcuff@gmail.com>
2018-08-07 00:00:42 -0400

    Removed StatusSearchFragment

--- app/src/main/java/com/camtech/android/tweetbot/models/Keys.java
+++ app/src/main/java/com/camtech/android/tweetbot/models/Keys.java
@@ -1,8 +1,8 @@
 package com.camtech.android.tweetbot.models;
 
 public final class Keys {
-    private Keys() {}
     // These keys should be private and will be different for everyone
+    private Keys() {}
     public static final String CONSUMER_KEY = "";
     public static final String CONSUMER_KEY_SECRET = "";
 }
